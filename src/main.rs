4273bdbbd0f519400cef72c2cad55add6083e193
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-05-30 21:20:52 -0700

    first

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
