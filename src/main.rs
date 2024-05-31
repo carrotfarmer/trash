420950391ac0ebde83511785b6c9f8807b5d9590
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-05-31 10:00:13 -0700

    test 2

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    println!("{}: command not found", input);
 }
