00b8af7083d2c8f9976acb4afe7a7ab1652a7dd9
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-05-31 18:56:37 -0700

    repl 2

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ use std::io::{self, Write};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
         println!("{}: command not found", input.trim());
     }
 }
