d7d4c16230a96dc52e5fca0c916a9d0690a34585
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-05-31 18:56:07 -0700

    repl

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,7 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim());
+    loop {
+        println!("{}: command not found", input.trim());
+    }
 }
