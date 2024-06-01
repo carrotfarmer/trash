2f39148d86d8b1b044843b610294bbe34265200c
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-05-31 19:23:49 -0700

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,29 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         let cmd = input.trim();
+        let cmd_vec: Vec<char> = cmd.chars().collect();
+        let cmd_type = cmd_vec.get(0);
+        let args = cmd_vec.get(1..).unwrap();
 
-        match cmd {
-            "exit 0" => exit(0),
+        match cmd_type.unwrap().to_string().as_str() {
+            "echo" => {
+                let str_form: String = args.iter().collect();
+                println!("{}", str_form);
+            }
+            "exit" => {
+                if let Some(&single_char) = args.get(0) {
+                    // Convert the character to a String
+                    let char_string = single_char.to_string();
+
+                    // Parse the String to an integer
+                    match char_string.parse::<i32>() {
+                        Ok(exit_code) => exit(exit_code),
+                        Err(_) => println!("exit: could not parse exit code"),
+                    }
+                } else {
+                    println!("exit: too many arguments");
+                }
+            }
             _ => println!("{}: command not found", cmd),
         }
     }
